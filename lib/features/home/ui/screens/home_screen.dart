1061b956a88b416cbad8f77cadf6796b023e9dda
lychakartem <artem.luchak@gmail.com>
2025-04-23 02:07:07 +0300

    logout function + hotfix

--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -1,18 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart'; 
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
+  Future<void> _signOut(BuildContext context) async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      if (context.mounted) { 
+          ScaffoldMessenger.of(context).showSnackBar(
+             const SnackBar(content: Text('Successfully logged out!')),
+          );
+      }
+    } catch (e) {
+      print('Error signing out: $e');
+       if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+             SnackBar(content: Text('Error logging out: $e')),
+          );
+       }
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Main Screen'), // Temporary title
         automaticallyImplyLeading: false, 
+        actions: [ 
+          IconButton(
+            icon: const Icon(Icons.logout), 
+            tooltip: 'Logout',
+            onPressed: () {
+              _signOut(context);
+            },
+          ),
+        ],
       ),
-      body: Center( 
+      body: Center(
         child: Image.asset(
           'assets/images/wave_chibi.png', 
+           width: 200,
+           height: 200,
+           fit: BoxFit.contain,
         ),
       ),
     );
